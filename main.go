e68669d9367bf87c051b07bdb8fea1156a5e04f5
jpaola <jpaolababy@gmail.com>
2022-04-29 01:14:08 -0400

    Updates to getAlbumById and addAlbum using db. Removed global albums variable.

--- main.go
+++ main.go
@@ -10,15 +10,12 @@ import (
 )
 
 // album represents data about a record album.
-type album struct {
+type Album struct {
     ID     string  `json:"id"`
     Title  string  `json:"title"`
     Artist string  `json:"artist"`
     Price  float64 `json:"price"`
 }
-
-// albums slice to seed record album data.
-var albums = []album{}
 
 // Database credentials
 var dbuser = "nameOfUser"
@@ -27,7 +24,7 @@ var dbname = "nameOfDB"
 
 
 // getAlbums responds with the list of all albums as JSON.
-func getAlbums(context *gin.Context) {
+func GetAlbums(ctx *gin.Context) {
 	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@tcp(127.0.0.1:3306)/"+dbname)
 
 	// if there is an error opening the connection, handle it
@@ -47,8 +44,11 @@ func getAlbums(context *gin.Context) {
 		return
 	}
 	
+	// albums slice to seed record album data.
+ 	albums := []Album{}
+
 	for results.Next() {
-		var a album
+		var a Album
 		// for each row, scan into the album struct
 		err = results.Scan(&a.ID, &a.Title, &a.Artist, &a.Price)
 
@@ -59,44 +59,90 @@ func getAlbums(context *gin.Context) {
 		albums = append(albums, a)
 	}
 
-    context.IndentedJSON(http.StatusOK, albums)
+    ctx.IndentedJSON(http.StatusOK, albums)
 }
 
 // addAlbum adds an album from JSON received in the request body.
-func addAlbum(context *gin.Context) {
-    var newAlbum album
+func AddAlbum(ctx *gin.Context) {
+	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@tcp(127.0.0.1:3306)/"+dbname)
 
-    // Call BindJSON to bind the received JSON to
-    // newAlbum.
-    if error := context.BindJSON(&newAlbum); error != nil {
-        return
-    }
+    var newAlbum Album
 
-    // Add the new album to the slice.
-    albums = append(albums, newAlbum)
-    context.IndentedJSON(http.StatusCreated, newAlbum)
+	if err := ctx.ShouldBindJSON(&newAlbum); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		// simply print the error to the console
+		fmt.Println("DB Connection error", err.Error())
+		return
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(
+		"INSERT INTO albums (albumId,title,artist,price) VALUES (?,?,?,?)",
+		newAlbum.ID, newAlbum.Title, newAlbum.Artist, newAlbum.Price)
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		fmt.Println("An error occured while attempting to insert new album to db", err.Error())
+	}
+
+	defer insert.Close()
+
+ 	ctx.JSON(http.StatusOK, gin.H{"data": insert})
 }
 
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
-func getAlbumByID(context *gin.Context) {
-    id := context.Param("id")
+func GetAlbumByID(ctx *gin.Context) {
+	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@tcp(127.0.0.1:3306)/"+dbname)
 
-    // Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, album := range albums {
-        if album.ID == id {
-            context.IndentedJSON(http.StatusOK, album)
-            return
-        }
-    }
-    context.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	id := ctx.Param("id")
+
+	// album to seed record album data.
+ 	album := &Album{}
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		// simply print the error to the console
+		fmt.Println("DB Connection error", err.Error())
+		return
+	}
+
+	defer db.Close()
+
+	newAlbum, err := db.Query("SELECT * FROM albums where albumId=?", id)
+
+	// if there is an error during query, handle it
+	if err != nil {
+		fmt.Println("Query Error", err.Error())
+		return
+	}
+	 
+	if newAlbum.Next() {
+		err = newAlbum.Scan(&album.ID, &album.Title, &album.Artist, &album.Price)
+		if err != nil {
+			fmt.Println("Error while scanning album struct", err.Error()) // proper error handling instead of panic in your app
+		} 
+	} else {
+		ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	}
+
+	fmt.Println(album)
+	ctx.IndentedJSON(http.StatusOK, album)
 }
 
 // removeAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then removes that album as a response.
-func removeAlbumByID(context *gin.Context) {
-    id := context.Param("id")
+func RemoveAlbumByID(ctx *gin.Context) {
+    id := ctx.Param("id")
+
+	// albums slice to seed record album data.
+ 	albums := []Album{}
 
 	// Loop over the list of albums, looking for
     // an album whose ID value matches the parameter.
@@ -105,22 +151,25 @@ func removeAlbumByID(context *gin.Context) {
 			copy(albums[i:], albums[i+1:]) // Shift a[i+1:] left one index.
 			// albums[len(albums)-1] = {""}     // Erase last element (write zero value).
 			albums = albums[:len(albums)-1]     // Truncate slice.
-            context.IndentedJSON(http.StatusOK, album)
+            ctx.IndentedJSON(http.StatusOK, album)
             return
         }
     }
-    context.IndentedJSON(http.StatusNotFound, gin.H{"message": "album could not be deleted"})
+    ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album could not be deleted"})
 }
 
 // updateAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then removes that album as a response.
-func updateAlbumByID(context *gin.Context) {
-    var newAlbum album
-    id := context.Param("id")
+func UpdateAlbumByID(ctx *gin.Context) {
+    var newAlbum Album
+    id := ctx.Param("id")
+
+	// albums slice to seed record album data.
+ 	albums := []Album{}
 
     // Call BindJSON to bind the received JSON to
     // newAlbum.
-    if error := context.BindJSON(&newAlbum); error != nil {
+    if error := ctx.BindJSON(&newAlbum); error != nil {
         return
     }
 
@@ -129,11 +178,11 @@ func updateAlbumByID(context *gin.Context) {
     for i, album := range albums {
         if album.ID == id {
 			albums[i] = newAlbum // update the album by id
-            context.IndentedJSON(http.StatusOK, album)
+            ctx.IndentedJSON(http.StatusOK, album)
             return
         }
     }
-    context.IndentedJSON(http.StatusNotFound, gin.H{"message": "album could not be updated"})
+    ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album could not be updated"})
 }
 
 func main() {
@@ -141,11 +190,11 @@ func main() {
 	// initialize gin router using Default
     router := gin.Default()
 
-	router.GET("/albums", getAlbums)
-    router.GET("/albums/:id", getAlbumByID)
-    router.POST("/albums", addAlbum)
-    router.DELETE("/albums/:id", removeAlbumByID)
-    router.PUT("/albums/:id", updateAlbumByID)
+	router.GET("/albums", GetAlbums)
+    router.GET("/albums/:id", GetAlbumByID)
+    router.POST("/albums", AddAlbum)
+    router.DELETE("/albums/:id", RemoveAlbumByID)
+    router.PUT("/albums/:id", UpdateAlbumByID)
 
     router.Run("localhost:8080")
 }
